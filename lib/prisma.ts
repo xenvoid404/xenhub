a19e65f032a20b0ee49d4524e7132dc2be61de38
xenvoid404 <xenvoid404@gmail.com>
2025-09-17 16:47:39 +0700

    menghapus package dan kode prisma

--- lib/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.APP_ENV !== 'production') globalForPrisma.prisma = prisma;
