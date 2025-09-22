74e81500bd394b693929d9308e6110422c402a32
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 18:17:51 +0700

    memperbaiki import reactnode di app/(main)/layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@/app/(main)/components/header';
 
-export default function MainLayout({ children }: { children: React.ReacNode }) {
+export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Header />
