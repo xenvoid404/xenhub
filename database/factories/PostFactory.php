762313a3884192dafa2eab8c66d6cab30592904c
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 15:13:26 +0000

    menambahkan seeder tabel kategori

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,7 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Enums\Post\Status;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,8 +21,17 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = $this->faker->sentence();
+
         return [
-            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'sub_category_id' => SubCategory::factory(),
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => $this->faker->paragraph(),
+            'is_featured' => $this->faker->boolean(),
+            'status' => $this->faker->randomElement(Status::cases()),
         ];
     }
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  public function run(): void
+  {
+    Category::create([
+      "name" => "Teknologi",
+      "slug" => "teknologi",
+    ]);
+  }
 }
