817c6ea115935925b1c3aeae188ac6388b083f1e
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 23:20:24 +0000

    mengubaj z index sidebar supaya di bawah header

--- app/(main)/components/sidebar.tsx
+++ app/(main)/components/sidebar.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
 import { useSidebarStore } from '@/store/sidebar-store';
 import Link from 'next/link';
@@ -7,7 +6,7 @@ import Link from 'next/link';
 const navItems = [
     { name: 'Artikel Pilihan', href: '#featured-post' },
     { name: 'Artikel Terbaru', href: '#recent-post' },
-    { name: 'Artikel Populer', href: '#popular-post' },
+    { name: 'Artikel Populer', href: '#popular-post' }
 ];
 
 export function Sidebar() {
@@ -15,7 +14,7 @@ export function Sidebar() {
 
     return (
         <Sheet open={isOpen} onOpenChange={close}>
-            <SheetContent className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 p-0 pt-14">
+            <SheetContent className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 p-0">
                 <div className="p-6">
                     <SheetHeader className="text-left mb-8">
                         <SheetTitle>Menu</SheetTitle>
