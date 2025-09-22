03a2c9208fd27aec68dd6869e1ad3702a44e9cf2
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 15:18:44 +0000

    menambahkan seeder tabel user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\User\Role;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -9,6 +11,14 @@ class UserSeeder extends Seeder
 {
   public function run(): void
   {
-    //
+    User::create([
+      "name" => "Xenvoid",
+      "username" => "xenvoid",
+      "avatar" => null,
+      "email" => "xenvoid@xenhub.com",
+      "email_verified_at" => now(),
+      "password" => bcrypt("123@Xenvoid"),
+      "role" => Role::ADMINISTRATOR,
+    ]);
   }
 }
