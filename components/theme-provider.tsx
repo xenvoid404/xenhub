90ad6a8a235c65359f27493d6a8cdeb3081f776e
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 16:49:45 +0700

    menginstall next-themes

---
+++ components/theme-provider.tsx
@@ -0,0 +1,5 @@
+import { ThemeProvider as NextThemeProvider, type ThemeProviderProps } from 'next-themes';
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}
