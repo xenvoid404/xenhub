d953a2cc93a22bd434edf00f5c73a65588685fe5
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 19:15:11 +0700

    mengubah header dari sticky menjadi fixed

--- app/(main)/components/header.tsx
+++ app/(main)/components/header.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from '@/components/theme-toggle';
 
 export function Header() {
     return (
-        <header className="sticky top-0 z-50 flex flex-col border-b border-muted bg-background backdrop-blur-lg">
+        <header className="fixed w-full top-0 z-50 flex flex-col border-b border-muted bg-background backdrop-blur-lg">
             <div className="flex h-14 items-center justify-between px-4 sm:px-6">
                 <div className="flex h-full items-center">
                     <Link href="/" aria-label={`${process.env.APP_NAME} - Go to homepage`}>
