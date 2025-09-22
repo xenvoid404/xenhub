8d3b22c460cd065825a7d476d48e8e39df9b43c9
xenvoid404 <xenvoid404@gmail.com>
2025-09-22 15:15:26 +0000

    menambahkan seeder tabel sub kategori

--- database/seeders/SubCategorySeeder.php
+++ database/seeders/SubCategorySeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\SubCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class SubCategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  public function run(): void
+  {
+    SubCategory::create([
+      "category_id" => 1,
+      "name" => "Artificial Intelligence",
+      "slug" => "artificial-intelligence",
+    ]);
+    SubCategory::create([
+      "category_id" => 1,
+      "name" => "Programming",
+      "slug" => "programming",
+    ]);
+  }
 }
