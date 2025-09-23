d8cc05109c79de17b3e2a3dc65b587521547fa84
xenvoid404 <xenvoid404@gmail.com>
2025-09-24 01:34:06 +0700

    menambahkan response kategori, subcategory untuk featured post

--- app/Http/Controllers/Api/V1/User/Post/FeaturedPostController.php
+++ app/Http/Controllers/Api/V1/User/Post/FeaturedPostController.php
@@ -11,12 +11,26 @@ class FeaturedPostController extends Controller
 {
   public function index()
   {
-    $posts = Post::with(["user:id,name,avatar,role"])
+    $posts = Post::with(["user:id,name,avatar,role", "category:id,name,slug", "subCategory:id,name,slug"])
       ->where("is_featured", true)
       ->where("status", Status::PUBLISHED)
       ->orderBy("created_at", "desc")
       ->limit(6)
       ->get();
+
+    $transformedPosts = $posts->map(function ($post) {
+      return [
+        'id' => $post->id,
+        'title' => $post->title,
+        'slug' => $post->slug,
+        'description' => $post->excerpt,
+        'category' => $post->category,
+        'subCategory' => $post->subCategory,
+        'author' => $post->user->name,
+        'date' => $post->created_at->format('d M Y'),
+        'user' => $post->user,
+      ];
+    });
 
     return response()->json([
       "meta" => [
@@ -25,7 +39,7 @@ class FeaturedPostController extends Controller
         "message" => "Getting featured post successful",
       ],
       "data" => [
-        "posts" => $posts,
+        "posts" => $transformedPosts,
       ],
     ]);
   }
