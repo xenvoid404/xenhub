b27f596e994d212312e6f55ef340947d26796847
xenvoid404 <xenvoid404@gmail.com>
2025-09-24 08:36:31 +0700

    refactor sidebar

--- app/(main)/components/sidebar.tsx
+++ app/(main)/components/sidebar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
 const navItems = [
+    { name: 'Beranda', href: '/' },
     { name: 'Artikel Pilihan', href: '#featured-post' },
     { name: 'Artikel Terbaru', href: '#recent-post' },
     { name: 'Artikel Populer', href: '#popular-post' }
@@ -15,22 +16,24 @@ export function Sidebar() {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 z-40 bg-background" role="dialog" aria-modal="true" aria-label="Sidebar menu">
-            <div className="flex flex-col h-full">
-                <div className="flex flex-1 flex-col items-center justify-start pt-24 px-4 sm:px-6">
-                    <nav className="flex flex-col items-center gap-6 w-full">
-                        <div className="flex flex-col space-y-4">
-                            {navItems.map(item => (
-                                <Button key={item.name} variant="ghost" className="block" asChild>
-                                    <Link href={item.href} onClick={close}>
-                                        {item.name}
-                                    </Link>
-                                </Button>
-                            ))}
-                        </div>
+        <div
+            className="fixed inset-0 z-40 bg-background transform transition-transform duration-300 ease-in-out"
+            role="dialog"
+            aria-modal="true"
+            aria-label="Sidebar menu"
+        >
+            <div className="relative flex flex-col h-full">
+                <div className="flex items-center justify-between pt-18 p-2 sm:p-6">
+                    <nav className="flex-1 space-y-2">
+                        {navItems.map(item => (
+                            <Button key={item.name} variant="ghost" className="block" asChild>
+                                <Link href={item.href} onClick={close}>
+                                    {item.name}
+                                </Link>
+                            </Button>
+                        ))}
                     </nav>
                 </div>
-                <nav></nav>
             </div>
         </div>
     );
